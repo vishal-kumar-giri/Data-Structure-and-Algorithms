89a66ba3766554eaafe1380237e882d4529610a7
vishal-kumar-giri <vg799233@gmail.com>
2025-08-31 12:18:28 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (26.61%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numTrees(int n) {
+        int[] count = new int[n + 1];
+        count[0] = 1;  // Empty tree
+        count[1] = 1;  // Single node tree
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                count[i] += count[j] * count[i - j - 1];
+            }
+        }
+        return count[n];
+    }
+}
