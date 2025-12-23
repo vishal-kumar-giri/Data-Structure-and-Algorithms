4f9fed8f2e8b45c16c33609be384d0f796dc7bf3
vishal-kumar-giri <vg799233@gmail.com>
2025-12-23 16:33:06 +0530

    Time: 3 ms (20.27%) | Memory: 47.8 MB (26.57%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0 && nums[j]==0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+            if(nums[j]!=0)j++;
+        }
+    }
+}
