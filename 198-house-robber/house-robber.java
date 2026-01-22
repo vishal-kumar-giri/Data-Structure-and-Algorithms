882e1037da9c527585b74d54dc65be8937a88fd1
vishal-kumar-giri <vg799233@gmail.com>
2026-01-22 17:55:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (44.40%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int rob(int[] nums) {
+      int even = 0;
+      int odd = 0;
+      if(nums.length==0)return 0;
+      for(int i=0;i<nums.length;i++){
+        if(i%2==0){
+            even = Math.max(even + nums[i], odd);
+        }else{
+            odd = Math.max(odd + nums[i], even);
+        }
+      }
+      return Math.max(even,odd);  
+    }
+}
