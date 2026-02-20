3a9c3a25a4b4587b3dd1142cf80d2cf6c18ceefe
vishal-kumar-giri <vg799233@gmail.com>
2026-02-20 22:18:12 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (93.21%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,14 +11,14 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head==null)return false;
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast!= null && fast.next!=null){
-            fast = fast.next.next;
-            slow = slow.next;
-            if(slow==fast)return true;
-        }
-        return false;
+       if(head==null || head.next ==null)return false;
+       ListNode slow = head;
+       ListNode fast = head;
+       while(fast!=null && fast.next!=null){
+        fast = fast.next.next;
+        slow = slow.next;
+        if(slow == fast)return true;
+       }
+       return false; 
     }
 }
