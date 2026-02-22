7aa2f6d98162151c6cce45a0e235fc441ac15816
vishal-kumar-giri <vg799233@gmail.com>
2026-02-22 17:11:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (70.83%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -18,9 +18,9 @@ class Solution {
        if(root==null)return true;
        return check(root.left,root.right); 
     }
-    public boolean check(TreeNode l,TreeNode r){
+    public boolean check(TreeNode l, TreeNode r){
         if(l==null && r==null)return true;
         if(l==null || r==null)return false;
-        return (l.val==r.val) &&check(l.left,r.right) && check(l.right,r.left);
+        return (l.val==r.val) && check(l.left,r.right) && check(l.right,r.left);
     }
 }
