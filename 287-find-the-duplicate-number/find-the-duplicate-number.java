89b03ea75bffa7b1062dd68c3ae299921f9c5263
vishal-kumar-giri <vg799233@gmail.com>
2026-06-02 17:15:16 +0530

    Time: 38 ms (11.11%) | Memory: 114.9 MB (5.00%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,12 +1,14 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-      HashMap<Integer,Integer> map = new HashMap<>();
-      for(int num:nums)map.put(num,map.getOrDefault(num,0)+1);
-      for(Map.Entry<Integer,Integer>entry:map.entrySet()){
-        int key = entry.getKey();
-        int freq = entry.getValue();
-        if(freq>=2)return key;
-      }
-      return -1;  
+        HashMap <Integer , Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num , map.getOrDefault(num , 0)+1);
+        }
+        for(Map.Entry <Integer,Integer> entry:map.entrySet()){
+            int key = entry.getKey();
+            int value = entry.getValue();
+            if(value >= 2)return key;
+        }
+        return -1;
     }
 }
