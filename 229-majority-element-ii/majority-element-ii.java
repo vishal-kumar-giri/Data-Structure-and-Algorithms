4bef691f47e15e8f91d8d5e3e478b0d454dac96a
vishal-kumar-giri <vg799233@gmail.com>
2026-06-02 17:42:56 +0530

    Time: 14 ms (41.45%) | Memory: 50.9 MB (98.70%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,21 +1,16 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer, Integer> countMap = new HashMap<>();
-        List<Integer> result = new ArrayList<>();
-        
-        // Count occurrences of each number
-        for (int num : nums) {
-            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
-        }
-        
-        // Check which numbers are majority elements
-        int threshold = nums.length / 3;
-        for (Integer key : countMap.keySet()) {
-            if (countMap.get(key) > threshold) {
-                result.add(key);
+        HashMap <Integer , Integer> map = new HashMap<>();
+        for(int num : nums)map.put(num , map.getOrDefault(num , 0) + 1);
+        int n = nums.length/3;
+        List <Integer> list = new ArrayList<>();
+        for(Map.Entry <Integer , Integer> entry :map.entrySet()){
+            int key = entry.getKey();
+            int freq = entry.getValue();
+            if(freq > n){
+                list.add(key);
             }
-        }
-        
-        return result;
+        }   
+        return list;
     }
 }
