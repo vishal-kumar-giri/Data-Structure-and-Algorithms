8fa2b1038a51ed4d0cce78cfe5ba7341939e0c2d
vishal-kumar-giri <vg799233@gmail.com>
2026-08-23 17:23:28 +0530

    Time: 7 ms (79.56%) | Memory: 46.4 MB (99.27%) - LeetSync

---
+++ 2039-sum-game/sum-game.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int half = n / 2;
+        int s1 = 0;
+        int s2 = 0;
+        int q1 = 0;
+        int q2 = 0;
+
+        for (int i = 0; i < half; i++) {
+            if (num.charAt(i) == '?') {
+                q1++;
+            } else {
+                s1 += num.charAt(i) - '0';
+            }
+        }
+        for (int i = half; i < n; i++) {
+
+            if (num.charAt(i) == '?') {
+                q2++;
+            } else {
+                s2 += num.charAt(i) - '0';
+            }
+        }
+        if ((q1 + q2) % 2 == 1) {
+            return true;
+        }
+        return s1 - s2 != 9 * (q2 - q1) / 2;
+    }
+}
