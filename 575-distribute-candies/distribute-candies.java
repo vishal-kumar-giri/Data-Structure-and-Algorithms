dcb2bd66c44c628dda2128ffaf20c1e2c78d9386
vishal-kumar-giri <vg799233@gmail.com>
2026-09-11 23:26:41 +0530

    Time: 31 ms (55.26%) | Memory: 48.6 MB (49.43%) - LeetSync

---
+++ 575-distribute-candies/distribute-candies.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int candy : candyType) {
+            set.add(candy);
+        }
+        return Math.min(set.size(), candyType.length / 2);
+    }
+}
